51c5cb8bd054ca0aff4330541396839e03c6239e
MAS4RADIA <113591699+MAS4RADIA@users.noreply.github.com>
2024-05-05 15:39:16 +0300

    modified:   Set/Room.js

--- Set/Room.js
+++ Set/Room.js
@@ -13,6 +13,8 @@ export function Switch ()
            {  path = "/introduction";  }
        if (path [0] == "/")
            {  path = path.slice (1);  }
+       if (path [path.length - 1] == "/")
+           {  path = path.slice (0, path.length - 1);  }
 
        room.path = path;
        SetNavigation (room);
