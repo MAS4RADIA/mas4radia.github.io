176a3ef579b04a12179bd17ae98cec66d02814a2
MAS4RADIA <113591699+MAS4RADIA@users.noreply.github.com>
2024-05-13 09:32:45 +0300

    modified:   Paint/Desktop/Content.css 	modified:   Paint/Desktop/Structure.css 	modified:   Set/Movement.js

--- Set/Movement.js
+++ Set/Movement.js
@@ -1,3 +1,6 @@
+window.addEventListener ("resize", ResizeWindow);
+
+var board;
 export function SetWindows ()
     {
        var list, item;
@@ -39,6 +42,7 @@ function SwitchActive (list = null)
        if (parent == undefined)
            {  return;  }
 
+       var dont_do_anything = false;
        old = parent.getElementsByClassName ("active");
        for (item in old)
            {
@@ -46,8 +50,16 @@ function SwitchActive (list = null)
                   {  continue;  }
 
               var element = old [item];
+              if (element == active)
+                  {
+                     dont_do_anything = true;
+                     continue;
+                   }
               element.classList.remove ("active");
             }
+       if (dont_do_anything)
+           {  return;  }
+
        active.classList.add ("active");
        OpenWindow (active);
      }
@@ -56,7 +68,7 @@ function OpenWindow (active = null)
        if (active == null)
            {  return;  }
 
-       var root, parent, board;
+       var root, parent;
        parent = active.parentNode;
        if (parent == undefined)
            {  return;  }
@@ -70,4 +82,22 @@ function OpenWindow (active = null)
            {  return;  }
 
        board.innerHTML = active.innerHTML;
+       ResizeWindow ();
+     }
+function ResizeWindow ()
+    {
+       if (board == null || board.tagName == undefined)
+           {  return;  }
+
+       var raw, width, clean, area, height;
+       width = board.clientWidth;
+       height = board.clientHeight;
+       if (isNaN (width) || isNaN (height))
+           {  return;  }
+
+       area = width * height;
+       raw = Math.sqrt (area / 2);
+       clean = Math.ceil (raw);
+       board.style.setProperty ("min-height", 0);
+       board.style.setProperty ("width", (clean * 2) + "px");
      }
